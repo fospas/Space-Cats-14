b9be0130c05daccc522d25b57094f7c92d6d6cd5
CrimeMoot <wakeafa@gmail.com>
2024-08-26 19:50:25 +0500

    add

--- Content.Server/Fax/FaxSystem.cs
+++ Content.Server/Fax/FaxSystem.cs
@@ -5,6 +5,7 @@ using Content.Server.DeviceNetwork;
 using Content.Server.DeviceNetwork.Components;
 using Content.Server.DeviceNetwork.Systems;
 using Content.Server.Labels;
+using Content.Server.Paper;
 using Content.Server.Popups;
 using Content.Server.Power.Components;
 using Content.Server.Tools;
