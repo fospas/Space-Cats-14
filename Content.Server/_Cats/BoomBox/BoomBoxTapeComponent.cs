8c6841cf080177386106587d2278ebd1d0cefdf1
CrimeMoot <wakeafa@gmail.com>
2024-10-09 15:04:02 +0500

    fix

--- Content.Server/_Cats/BoomBox/BoomBoxTapeComponent.cs
+++ Content.Server/_Cats/BoomBox/BoomBoxTapeComponent.cs
@@ -6,14 +6,20 @@ using Content.Shared.Containers.ItemSlots;
 
 namespace Content.Server._Cats.BoomBox;
 
+
 [RegisterComponent]
 public sealed partial class BoomBoxTapeComponent : Component
 {
+
     [DataField("soundPath")]
     public String SoundPath;
+
     [DataField("syndStatus")]
     public bool SyndStatus = false;
+
     [DataField("syndItem")]
     public String SyndItem;
+
     public bool Used = false;
+
 }
