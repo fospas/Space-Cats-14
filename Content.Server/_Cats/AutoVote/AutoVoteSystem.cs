5df28b4322111b5cdc82b8cd02a658e8a0632c59
CrimeMoot <wakeafa@gmail.com>
2024-11-24 18:28:35 +0000

    add autovote

---
+++ Content.Server/_Cats/AutoVote/AutoVoteSystem.cs
@@ -0,0 +1,54 @@
+using Robust.Shared.Configuration;
+using Content.Server.Voting.Managers;
+using Content.Shared.GameTicking;
+using Content.Shared.Voting;
+using Content.Shared._Cats.ССVars;
+using Robust.Server.Player;
+using Content.Server.GameTicking;
+
+namespace Content.Server._CorvaxNext.AutoVote;
+
+public sealed class AutoVoteSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] public readonly IVoteManager _voteManager = default!;
+    [Dependency] public readonly IPlayerManager _playerManager = default!;
+
+    public bool _shouldVoteNextJoin = false;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<RoundRestartCleanupEvent>(OnReturnedToLobby);
+        SubscribeLocalEvent<PlayerJoinedLobbyEvent>(OnPlayerJoinedLobby);
+    }
+
+    public void OnReturnedToLobby(RoundRestartCleanupEvent ev) => CallAutovote();
+
+    public void OnPlayerJoinedLobby(PlayerJoinedLobbyEvent ev)
+    {
+        if (!_shouldVoteNextJoin)
+            return;
+
+        CallAutovote();
+        _shouldVoteNextJoin = false;
+    }
+
+    private void CallAutovote()
+    {
+        if (!_cfg.GetCVar(ССVars.AutoVoteEnabled))
+            return;
+
+        if (_playerManager.PlayerCount == 0)
+        {
+            _shouldVoteNextJoin = true;
+            return;
+        }
+
+        if (_cfg.GetCVar(ССVars.MapAutoVoteEnabled))
+            _voteManager.CreateStandardVote(null, StandardVoteType.Map);
+        if (_cfg.GetCVar(ССVars.PresetAutoVoteEnabled))
+            _voteManager.CreateStandardVote(null, StandardVoteType.Preset);
+    }
+}
