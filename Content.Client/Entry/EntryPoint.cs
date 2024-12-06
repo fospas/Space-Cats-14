a906bcc7f7311b8ba2f5bf7ff1da14be4cf7aac9
CrimeMoot <wakeafa@gmail.com>
2024-12-06 23:21:02 +0500

    fix

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -178,7 +178,7 @@ namespace Content.Client.Entry
 
             _parallaxManager.LoadDefaultParallax();
 
-           _overlayManager.AddOverlay(new BasePostProcessOverlay());
+            _overlayManager.AddOverlay(new BasePostProcessOverlay());
             _overlayManager.AddOverlay(new SingularityOverlay());
             _overlayManager.AddOverlay(new RMCExplosionShockWaveOverlay()); // Ataraxia
             _overlayManager.AddOverlay(new RadiationPulseOverlay());
