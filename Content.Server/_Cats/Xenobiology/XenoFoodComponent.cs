91ecfe0b94163358f79d0e5f594c4da0bd355cf3
CrimeMoot <wakeafa@gmail.com>
2024-12-06 08:48:30 +0500

    Xenobiology

---
+++ Content.Server/_Cats/Xenobiology/XenoFoodComponent.cs
@@ -0,0 +1,8 @@
+using Content.Server._Cats.XenoBiology.Systems;
+
+namespace Content.Server._Cats.XenoFood.Components;
+
+[RegisterComponent]
+public sealed partial class XenoFoodComponent : Component
+{
+}
