627ccd060cecda107121f434f215104e761e8638
CrimeMoot <wakeafa@gmail.com>
2024-12-06 23:18:43 +0500

    add Additive lighting

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -26,4 +26,16 @@ public sealed partial class CCVars : CVars
     public static readonly CVarDef<bool> DebugPow3rDisableParallel =
         CVarDef.Create("debug.pow3r_disable_parallel", true, CVar.SERVERONLY);
 
+    /*
+     * GRAPHICS
+        */
+
+    /// <summary>
+    /// Toggle for non-gameplay-affecting or otherwise status indicative post-process effects, such additive lighting.
+    /// In the future, this could probably be turned into an enum that allows only disabling more expensive post-process shaders.
+    /// However, for now (mid-July of 2024), this only applies specifically to a particularly cheap shader: additive lighting.
+    /// </summary>
+    public static readonly CVarDef<bool> PostProcess =
+        CVarDef.Create("graphics.post_process", true, CVar.CLIENTONLY | CVar.ARCHIVE);
+
 }
