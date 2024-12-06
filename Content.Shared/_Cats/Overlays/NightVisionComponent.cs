d3458563fea7c462878160749015451d6a2aceaa
CrimeMoot <wakeafa@gmail.com>
2024-12-06 09:22:31 +0500

    Update NightVisionComponent.cs

--- Content.Shared/_Cats/Overlays/NightVisionComponent.cs
+++ Content.Shared/_Cats/Overlays/NightVisionComponent.cs
@@ -9,7 +9,7 @@ public sealed partial class NightVisionComponent : SwitchableOverlayComponent
 {
     public override string? ToggleAction { get; set; } = "ToggleNightVision";
 
-    public override Color Color { get; set; } = Color.FromHex("#98FB98");
+    public override Color Color { get; set; } = Color.White;
 }
 
 public sealed partial class ToggleNightVisionEvent : InstantActionEvent;
