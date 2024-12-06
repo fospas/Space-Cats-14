17359e479dbf7692fe9e793fefbcbf5a29fe92b9
CrimeMoot <wakeafa@gmail.com>
2024-12-06 09:45:23 +0500

    Update ThermalVisionComponent.cs

--- Content.Shared/_Cats/Overlays/ThermalVisionComponent.cs
+++ Content.Shared/_Cats/Overlays/ThermalVisionComponent.cs
@@ -7,8 +7,6 @@ namespace Content.Shared._Cats.Overlays;
 public sealed partial class ThermalVisionComponent : SwitchableOverlayComponent
 {
     public override string? ToggleAction { get; set; } = "ToggleThermalVision";
-
-    public override Color Color { get; set; } = Color.FromHex("#F84742");
 }
 
 public sealed partial class ToggleThermalVisionEvent : InstantActionEvent;
