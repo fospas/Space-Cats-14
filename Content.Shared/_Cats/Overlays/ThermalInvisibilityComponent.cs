62fc24612a868b27b8fc1bbb0705290c5be182a9
CrimeMoot <wakeafa@gmail.com>
2024-12-14 16:01:52 +0000

    removerhermal

--- Content.Shared/_Cats/Overlays/ThermalInvisibilityComponent.cs
+++
@@ -1,6 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared._Cats.Overlays;
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class ThermalInvisibilityComponent : SwitchableOverlayComponent;
